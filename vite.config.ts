a5a8da623bbaf355ef1cad55a35a2c42ed28b0e2
Fernando Bolivar <fbolivarb@gmail.com>
2026-01-17 21:13:02 -0500

    fix(test): downgrade jsdom to v22.1.0 to resolve ESM require errors

--- vite.config.ts
+++ vite.config.ts
@@ -9,10 +9,6 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
-    server: {
-      deps: {
-        inline: ['html-encoding-sniffer', 'jsdom'],
-      },
-    },
+
   },
 })
