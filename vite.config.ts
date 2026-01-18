15e34a6f7f37a5d244fef00869c1a1ce9758e3d5
Fernando Bolivar <fbolivarb@gmail.com>
2026-01-17 21:05:13 -0500

    fix(test): inline jsdom and add vitest types reference

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -10,7 +11,7 @@ export default defineConfig({
     setupFiles: './src/test/setup.ts',
     server: {
       deps: {
-        inline: ['html-encoding-sniffer'],
+        inline: ['html-encoding-sniffer', 'jsdom'],
       },
     },
   },
